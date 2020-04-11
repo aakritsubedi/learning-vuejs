89f86ef801e5724e65cf8be2ced8b55bb25dea7c
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-04-11 09:07:12 +0545

    Input Binding: Input[checkbox] Fields

--- src/components/20InputBinding/20AddBlog.vue
+++ src/components/20InputBinding/20AddBlog.vue
@@ -27,12 +27,31 @@
                 required
               ></textarea>
             </div>
+            <div class="form-group" id="blog-categories">
+              <input type="checkbox" value="Personal" v-model="blog.categories" />
+              <label for="personal">Personal</label>
+
+              <input type="checkbox" value="Travel" v-model="blog.categories" />
+              <label for="personal">Travel</label>
+
+              <input type="checkbox" value="Inspirational" v-model="blog.categories" />
+              <label for="personal">Inspirational</label>
+
+              <input type="checkbox" value="Educational" v-model="blog.categories" />
+              <label for="personal">Educational</label>
+
+              <input type="checkbox" value="Fun" v-model="blog.categories" />
+              <label for="personal">Fun</label>
+            </div>
           </form>
         </div>
         <div class="col-md-7" id="preview">
           <h4 class="font-weight-bold">Preview Blog</h4>
           <h3>{{ blog.title }}</h3>
           <p id="blog-content">{{ blog.content }}</p>
+          <ul id="preview-categories">
+            <li v-for="category in blog.categories" v-bind:key="category">{{ category }}</li>
+          </ul>
         </div>
       </div>
     </div>
@@ -46,7 +65,8 @@ export default {
     return {
       blog: {
         title: "",
-        content: ""
+        content: "",
+        categories: []
       }
     };
   }
@@ -57,7 +77,32 @@ export default {
 #preview {
   border: 1px dotted #3a3a3a;
 }
+#preview-categories {
+  list-style: none;
+}
+#preview-categories li {
+  float: left;
+  width: auto;
+  margin: 0 5px;
+  padding: 2px 4px;
+  border: 1px solid #2C3335;
+}
+#preview-categories li:hover {
+  color: #ffffff;
+  background-color: #2C3335;
+  border: 1px dotted #DAE0E2;
+}
+
 #blog-content {
   text-indent: 25px;
+}
+#blog-categories input {
+  text-align: center;
+  display: inline-block;
+  margin: 0 3px 0 10px;
+}
+#blog-categories label {
+  font-weight: blod;
+  display: inline-block;
 }
 </style>
